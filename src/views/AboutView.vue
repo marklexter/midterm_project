f27bc8e6e726ee143570a93c478fe4b6af9db200
marklexter <100121227+marklexter@users.noreply.github.com>
2022-09-11 21:37:26 +0700

    demo for git

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,112 @@
 <template>
-    <v-card>
-        <v-title>
-            HOME
-        </v-title>
-    </v-card>
+    <br/><br/> 
+
+<v-card class="mx-auto">
+
+    <div id="pos"></div>
+
+    <center>
+
+        <v-btn text @click.stop="dialog= true" class color="primary" elevation="2">About Me</v-btn>
+      
+
+
+      <v-dialog v-model="dialog" max-width="600px">
+          <v-card>
+            
+              <v-card-title>
+                  <h2>About Me</h2>
+              </v-card-title>
+              
+
+          
+      
+              <v-card-text>
+
+          
+                  It displays the information about the Developer
+           
+              </v-card-text>
+          </v-card>
+      </v-dialog><br/><br/> 
+      <v-btn text @click.stop="dialog2 = true" class color="primary" elevation="2">Basic Math</v-btn>
+
+<v-dialog v-model="dialog2" max-width="600px">
+  <v-card>
+      <v-card-title>
+          <h2>Basic Math</h2>
+      </v-card-title>
+      <v-card-text>
+          A simple calculator that will dipslay the sum, difference,product, quotient and the average.
+      </v-card-text>
+  </v-card>
+</v-dialog><br/><br/> 
+
+<v-btn text @click.stop="dialog3 = true" class color="primary" elevation="2">String App</v-btn>
+
+      <v-dialog v-model="dialog3" max-width="600px">
+          <v-card>
+              <v-card-title>
+                  <h2>String App</h2>
+              </v-card-title>
+              <v-card-text>
+                  It displays the number of vowels the number the consonat and lastly for the converted vowels
+              </v-card-text>
+          </v-card>
+      </v-dialog><br/><br/> 
+
+      <v-btn text @click.stop="dialog4 = true" class color="primary" elevation="2">Vuetify</v-btn>
+
+      <v-dialog v-model="dialog4" max-width="600px">
+          <v-card>
+              <v-card-title>
+                  <h2>Vuetify</h2>
+              </v-card-title>
+              <v-card-text>
+                  It displays information details about Vuetify
+              </v-card-text>
+          </v-card>
+      </v-dialog><br/><br/> 
+
+      <v-btn text @click.stop="dialog5 = true" class color="primary" elevation="2">Vue Quiz</v-btn>
+
+      <v-dialog v-model="dialog5" max-width="600px">
+          <v-card>
+              <v-card-title>
+                  <h2>Vue Quiz</h2>
+              </v-card-title>
+              <v-card-text>
+                  A simple quiz about Mythology
+              </v-card-text>
+          </v-card>
+      </v-dialog><br/><br/> 
+
+
+
+
+   
+
+
+    </center>
+
+</v-card>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                dialog: false,
+                dialog2: false,
+                dialog3: false,
+                dialog4: false,
+                dialog5: false,
+            }
+        }
+    }
+    </script>
+    <style scoped>
+    #pos{
+        margin-top: 30px;
+    
+    }
+    </style>
